657639fac726e9e89634298e3828c7b0b98d4f52
Marek Bareła <marek.barela@wp.pl>
2019-10-25 00:21:06 +0200

    PricePanel component basic implementation + connection with store

--- src/components/Converter/components/PricePanel/PricePanel.tsx
+++ src/components/Converter/components/PricePanel/PricePanel.tsx
@@ -1,15 +1,44 @@
-import React from "react";
+import React, { useState, ChangeEvent, FC } from "react";
+import { getCurrencyData } from "../../Converter-selector";
+import { CurrencyData } from "../../Converter-model";
+import { connect } from "react-redux";
+import { RootState } from "../../../../redux/root-reducer";
 import styles from "./PricePanel.module.css";
 
-const PricePanel = () => {
+interface StateProps {
+  currencyData: CurrencyData;
+}
+
+type Props = StateProps;
+
+const PricePanel: FC<Props> = ({ currencyData }) => {
+  const [amount, setAmount] = useState("1");
+
+  const handleAmountChange = (e: ChangeEvent<HTMLInputElement>): void => {
+    const { value } = e.target;
+    setAmount(value);
+    if (!Number(value)) return;
+  };
+
   const { panel } = styles;
   return (
     <div className={panel}>
-      <input type="number" />
+      <input
+        type="number"
+        value={amount}
+        onChange={e => handleAmountChange(e)}
+      />
       <span>=</span>
       <p>Value</p>
     </div>
   );
 };
 
-export default PricePanel;
+const mapStateToProps = (state: RootState) => ({
+  currencyData: getCurrencyData(state)
+});
+
+export default connect<StateProps, {}, {}, RootState>(
+  mapStateToProps,
+  {}
+)(PricePanel);
