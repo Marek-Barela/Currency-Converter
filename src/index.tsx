f15e55eab1866d9490d35bd47432cd7777c22169
Marek Bareła <marek.barela@wp.pl>
2019-10-24 17:20:18 +0200

    basic redux redux-saga setup

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 import * as serviceWorker from "./serviceWorker";
+import { Provider } from "react-redux";
+import store from "./redux/store";
 import "./index.css";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
 
 serviceWorker.unregister();
