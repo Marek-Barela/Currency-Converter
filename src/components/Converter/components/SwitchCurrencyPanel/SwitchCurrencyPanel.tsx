91eadbb24656abfe1f9c051a274141a490fb8bc1
Marek Bareła <marek.barela@wp.pl>
2019-10-24 23:02:49 +0200

    set actual currency functionality implemented

--- src/components/Converter/components/SwitchCurrencyPanel/SwitchCurrencyPanel.tsx
+++ src/components/Converter/components/SwitchCurrencyPanel/SwitchCurrencyPanel.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from "react";
+import React, { FC, useState, ChangeEvent } from "react";
 import SwitchCurrencyPanelSelector from "../SwitchCurrencyPanelSelector";
 import SwitchCurrencyPanelSwitch from "../SwitchCurrencyPanelSwitch";
 import { CurrencyData } from "../../Converter-model";
@@ -16,15 +16,45 @@ type Props = StateProps;
 
 const SwitchCurrencyPanel: FC<Props> = ({ currencyData }) => {
   const { panel } = styles;
+  const [selectorsData, setSelectorsData] = useState({
+  /**  
+    * When user first time will enter to application, the default state will be set to SEK and PLN.
+    * When user will use application he will set new localStorage item
+    * Every next time the default state will be taked from local storage,
+    * so if user will reload page the last searching result will be displayed
+    */
+    currencyFrom: localStorage.getItem("currencyFrom") || "SEK",
+    currencyTo: localStorage.getItem("currencyTo") || "PLN"
+  });
+  const { currencyFrom, currencyTo } = selectorsData;
 
-  // convert keys to values { EUR : 4.123, PLN; 1.001, ... } => ["EUR", "PLN", ...]
+  // convert keys to values { EUR : 4.123, PLN: 1.001, ... } => ["EUR", "PLN", ...]
   const createListOfCurrencies = keys(currencyData.rates);
+
+  const onChange = (e: ChangeEvent<HTMLSelectElement>):void => {
+    const { name, value } = e.target;
+    setSelectorsData({
+      ...selectorsData,
+      [name]: value
+    });
+    localStorage.setItem(name, value)
+  };
 
   return (
     <div className={panel}>
-      <SwitchCurrencyPanelSelector options={createListOfCurrencies} />
+      <SwitchCurrencyPanelSelector
+        options={createListOfCurrencies}
+        name="currencyFrom"
+        value={currencyFrom}
+        onChange={onChange}
+      />
       <SwitchCurrencyPanelSwitch />
-      <SwitchCurrencyPanelSelector options={createListOfCurrencies} />
+      <SwitchCurrencyPanelSelector
+        options={createListOfCurrencies}
+        name="currencyTo"
+        value={currencyTo}
+        onChange={onChange}
+      />
     </div>
   );
 };
--- src/components/Converter/components/SwitchCurrencyPanelSelector/SwitchCurrencyPanelSelector.tsx
+++ src/components/Converter/components/SwitchCurrencyPanelSelector/SwitchCurrencyPanelSelector.tsx
@@ -1,14 +1,22 @@
-import React, { FC } from "react";
+import React, { FC, ChangeEvent } from "react";
 
 interface ParentProps {
   options: string[];
+  name: string;
+  value: string;
+  onChange: (event: ChangeEvent<HTMLSelectElement>) => void;
 }
 
 type Props = ParentProps;
 
-const SwitchCurrencyPanelSelector: FC<Props> = ({ options }) => {
+const SwitchCurrencyPanelSelector: FC<Props> = ({
+  options,
+  name,
+  value,
+  onChange
+}) => {
   return (
-    <select>
+    <select name={name} value={value} onChange={onChange}>
       {options.map((option, index) => {
         return <option key={index}>{option}</option>;
       })}
