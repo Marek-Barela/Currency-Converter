0d51362f080f36b913fba5c620ea55ccfb24a940
Marek Bareła <marek.barela@wp.pl>
2019-10-24 23:21:43 +0200

    small naming convention update

--- src/components/Converter/components/SwitchCurrencyPanel/SwitchCurrencyPanel.tsx
+++ src/components/Converter/components/SwitchCurrencyPanel/SwitchCurrencyPanel.tsx
@@ -1,6 +1,6 @@
 import React, { FC, useState, ChangeEvent } from "react";
-import SwitchCurrencyPanelSelector from "../SwitchCurrencyPanelSelector";
-import SwitchCurrencyPanelSwitch from "../SwitchCurrencyPanelSwitch";
+import CurrencyPanelSelector from "../SwitchCurrencyPanelSelector";
+import SwitchCurrencyButton from "../SwitchCurrencyPanelSwitch";
 import { CurrencyData } from "../../Converter-model";
 import { getCurrencyData } from "../../Converter-selector";
 import { connect } from "react-redux";
@@ -17,39 +17,39 @@ type Props = StateProps;
 const SwitchCurrencyPanel: FC<Props> = ({ currencyData }) => {
   const { panel } = styles;
   const [selectorsData, setSelectorsData] = useState({
-  /**  
-    * When user first time will enter to application, the default state will be set to SEK and PLN.
-    * When user will use application he will set new localStorage item
-    * Every next time the default state will be taked from local storage,
-    * so if user will reload page the last searching result will be displayed
-    */
+    /**
+     * When user first time will enter to application, the default state will be set to SEK and PLN.
+     * When user will use application he will set new localStorage item
+     * Every next time the default state will be taked from local storage,
+     * so if user will reload page the last searching result will be displayed
+     */
     currencyFrom: localStorage.getItem("currencyFrom") || "SEK",
     currencyTo: localStorage.getItem("currencyTo") || "PLN"
   });
   const { currencyFrom, currencyTo } = selectorsData;
 
-  // convert keys to values { EUR : 4.123, PLN: 1.001, ... } => ["EUR", "PLN", ...]
+  // use ramda to convert keys to values { EUR : 4.123, PLN: 1.001, ... } => ["EUR", "PLN", ...]
   const createListOfCurrencies = keys(currencyData.rates);
 
-  const onChange = (e: ChangeEvent<HTMLSelectElement>):void => {
+  const onChange = (e: ChangeEvent<HTMLSelectElement>): void => {
     const { name, value } = e.target;
     setSelectorsData({
       ...selectorsData,
       [name]: value
     });
-    localStorage.setItem(name, value)
+    localStorage.setItem(name, value);
   };
 
   return (
     <div className={panel}>
-      <SwitchCurrencyPanelSelector
+      <CurrencyPanelSelector
         options={createListOfCurrencies}
         name="currencyFrom"
         value={currencyFrom}
         onChange={onChange}
       />
-      <SwitchCurrencyPanelSwitch />
-      <SwitchCurrencyPanelSelector
+      <SwitchCurrencyButton />
+      <CurrencyPanelSelector
         options={createListOfCurrencies}
         name="currencyTo"
         value={currencyTo}
