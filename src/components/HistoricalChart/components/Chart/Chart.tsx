e2e240265d1760a4bb318e393d6423f11f0bdbc9
Marek Bareła <marek.barela@wp.pl>
2019-10-25 20:57:31 +0200

    chart functionality commited

--- src/components/HistoricalChart/components/Chart/Chart.tsx
+++ src/components/HistoricalChart/components/Chart/Chart.tsx
@@ -1,7 +1,69 @@
-import React from "react";
+import React, { FC } from "react";
+import { Line } from "react-chartjs-2";
+import { getHistoricalData } from "../DatePanel/DataPanel-selector";
+import { HistoricalData } from "../DatePanel/DataPanel-model";
+import { connect } from "react-redux";
+import styles from "./Chart.module.css";
+import { keys, values } from "ramda";
+import { RootState } from "../../../../redux/root-reducer";
 
-const Chart = () => {
-  return <div>3</div>;
+interface StateProps {
+  historicalData: HistoricalData;
+}
+
+type Props = StateProps;
+
+const Chart: FC<Props> = ({ historicalData }) => {
+  const { panel } = styles;
+
+  // Server respond with unordered data so is important so sort data bevore displaying in chart
+  const sortedCurrencies: any = {};
+  keys(historicalData.rates)
+    .sort()
+    .forEach(key => {
+      sortedCurrencies[key] = historicalData.rates[key];
+    });
+
+  /**
+   *  Use ramda to get dates and values
+   *  extractListOfDates = { 2019-09-15: { SEK: 1.234 }, ... } => ["2019-09-15", "2019-09-16", ...]
+   *  extractListOfCurrencies = { 2019-09-15: { SEK: 1.234 }, ... } => [{ SEK: 1.234 }, { SEK: 1.234 }, ... ]
+   *  getListOfValues = [{ SEK: 1.234 }, { SEK: 1.534 } => [1.234, 1.534];
+   */
+  const extractListOfDates = keys(sortedCurrencies);
+  const extractListOfCurrencies = values(sortedCurrencies);
+  const getListOfValues = extractListOfCurrencies.map(currency => values(currency)[0]);
+
+  return (
+    <div className={panel}>
+      <Line
+        data={{
+          labels: extractListOfDates,
+          datasets: [
+            {
+              label: "Currency",
+              data: getListOfValues,
+              pointHoverBackgroundColor: "#000000",
+              pointHoverRadius: 6,
+              backgroundColor: ["rgba(0, 0, 0, 0.5)"],
+              borderColor: ["#000000"],
+              borderWidth: 3
+            }
+          ]
+        }}
+        width={200}
+        height={400}
+        options={{ maintainAspectRatio: false }}
+      />
+    </div>
+  );
 };
 
-export default Chart;
+const mapStateToProps = (state: RootState) => ({
+  historicalData: getHistoricalData(state)
+});
+
+export default connect(
+  mapStateToProps,
+  {}
+)(Chart);
