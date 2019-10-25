f630495318e43a75dd044f534cc9cb5e559de167
Marek Bareła <marek.barela@wp.pl>
2019-10-26 01:11:46 +0200

    footer commited

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,13 +1,17 @@
 import React from "react";
 import Converter from "../Converter";
 import HistoricalChart from "../HistoricalChart";
+import Footer from "../Footer";
 
 const App = () => {
   return (
-    <main>
-      <Converter />
-      <HistoricalChart />
-    </main>
+    <>
+      <main>
+        <Converter />
+        <HistoricalChart />
+      </main>
+      <Footer />
+    </>
   );
 };
 
